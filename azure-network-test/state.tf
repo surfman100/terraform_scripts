36214da9874bda9f9efa74367b867470747fe3cd
Bill Tierney <billtierneyireland@gmail.com>
2025-09-22 11:04:09 +0100

    adding state config file

--- azure-network-test/state.tf
+++ azure-network-test/state.tf
@@ -1,9 +1,9 @@
 # state.tf
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatedwaqq"
+    container_name       = "tfstate"
     key                  = ""
   }
 }
